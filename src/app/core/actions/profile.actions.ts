5d2ea271ad70b5cf35c06fca2c699637921b7236
Falkai Péter <formal.pfalkai@gmail.com>
2020-03-28 18:45:42 +0100

    profile state, and actions, first, deploy, rewrite some cloud function some minor change in design

---
+++ src/app/core/actions/profile.actions.ts
@@ -0,0 +1,38 @@
+import { WlUser } from '@wl-core/models/user.model';
+
+export namespace WLProfileActions {
+
+  export namespace Set {
+
+    export class ByID {
+      static readonly type = '[profile] byID';
+      constructor(public uid: string) {}
+    }
+    export class ByModel {
+      static readonly type = '[profile] byModel';
+      constructor(public model: WlUser.Public) {}
+    }
+
+    export class Followed {
+      static readonly type = '[profile] followedByValue';
+    }
+
+  }
+
+  export class Follow {
+    static readonly type = '[profile] follow';
+  }
+
+  export class FollowedFromDb {
+    static readonly type = '[profile] followedByValue';
+    constructor(public currentUserID: string, public userID: string) {}
+  }
+
+  export class Followers {
+    static readonly type = '[profile] followers';
+  }
+  export class Followings {
+    static readonly type = '[profile] followings';
+  }
+
+}
