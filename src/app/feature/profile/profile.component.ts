e53ba65f4ef91f5d45b5eecd1449f3b2ff38d39f
Falkai Péter <formal.pfalkai@gmail.com>
2020-04-03 19:53:36 +0200

    design finished on mobile

--- src/app/feature/profile/profile.component.ts
+++ src/app/feature/profile/profile.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Store, Select } from '@ngxs/store';
-import { ProfileState, ProfileStateModel } from '@wl-core/states/profile.state';
+import { ProfileStateModel } from '@wl-core/states/profile.state';
 import { Observable } from 'rxjs';
 import { UserService } from '@wl-shared/services/user/user.service';
 import { WLProfileActions } from '@wl-core/actions/profile.actions';
-import { CurrentUserState } from '@wl-core/states/current-user.state';
+import { BreakpointState, Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile',
@@ -14,31 +15,37 @@ import { CurrentUserState } from '@wl-core/states/current-user.state';
 })
 export class ProfileComponent implements OnInit {
   @Select() profile$: Observable<ProfileStateModel>;
-  mapOpened = true;
+  mapOpened;
   aboutOpened = false;
-  constructor(private store: Store, private userService: UserService) {
+  isMobile$;
+  isFullscreen = false;
+
+
+  /* A CONSTRUCTOR BEALLITJA AZT HOGY HA MOBILON (<=425PX) VAN AZ OLDAL AKKOR A MAP NE JELENJEN MEG ALAPBOL */
+  constructor(private store: Store, private userService: UserService, breakpointObserver: BreakpointObserver) {
+    this.isMobile$ = breakpointObserver.observe(['(max-width: 425px)']).pipe(map(result => result.matches));
+    this.isMobile$.subscribe(result => this.mapOpened = !result);
   }
 
   ngOnInit() {
   }
-
+ /* FALSERA ALLITJA AZ ABOOUTOT, ES HA A MAP NINCS MEGNYITVA AKKOR MEGNYITJA, ELLENKEZP ESETBEN BEZARJA AZT */
   openMap() {
-    this.mapOpened = true;
     this.aboutOpened = false;
+    this.mapOpened ? this.mapOpened = false : this.mapOpened = true;
   }
+  /* SAME AS MAP CSAK ABOUTRA */
   openAbout() {
     this.mapOpened = false;
-    this.aboutOpened = true;
+    this.aboutOpened ? this.aboutOpened = false : this.aboutOpened = true;
   }
 
   follow() {
     this.store.dispatch(new WLProfileActions.Set.Followed(true));
-
   }
 
   unfollow() {
     this.store.dispatch(new WLProfileActions.Set.Followed(false));
-    //this.userService.unfollow(current, user.user);
   }
 
 }
