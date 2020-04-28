5040424f2be3733c8aa17d975a40e77486111216
Falkai Péter <formal.pfalkai@gmail.com>
2020-04-28 18:51:34 +0200

    temporary fixing map error code, in future need to make it working

--- src/app/feature/profile/profile.component.ts
+++ src/app/feature/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
 import { Store, Select } from '@ngxs/store';
 import { ProfileStateModel } from '@wl-core/states/profile.state';
 import { Observable } from 'rxjs';
@@ -6,6 +6,7 @@ import { UserService } from '@wl-shared/services/user/user.service';
 import { WLProfileActions } from '@wl-core/actions/profile.actions';
 import { BreakpointState, Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-profile',
@@ -14,6 +15,8 @@ import { map } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileComponent implements OnInit {
+
+  @ViewChild('asd', {static: true}) map: ElementRef;
   @Select() profile$: Observable<ProfileStateModel>;
   mapOpened;
   aboutOpened = false;
@@ -22,7 +25,7 @@ export class ProfileComponent implements OnInit {
 
 
   /* A CONSTRUCTOR BEALLITJA AZT HOGY HA MOBILON (<=425PX) VAN AZ OLDAL AKKOR A MAP NE JELENJEN MEG ALAPBOL */
-  constructor(private store: Store, private userService: UserService, breakpointObserver: BreakpointObserver) {
+  constructor(private store: Store, private userService: UserService, breakpointObserver: BreakpointObserver, private afs: AngularFirestore) {
     this.isMobile$ = breakpointObserver.observe(['(max-width: 425px)']).pipe(map(result => result.matches));
     this.isMobile$.subscribe(result => this.mapOpened = !result);
   }
@@ -46,6 +49,11 @@ export class ProfileComponent implements OnInit {
 
   unfollow() {
     this.store.dispatch(new WLProfileActions.Set.Followed(false));
+  }
+
+
+  loadPlan() {
+    console.log('asd')
   }
 
 }
