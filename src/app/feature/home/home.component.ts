79a329c2cfc21999eec8f934dfe7e71608015017
Falkai Péter <formal.pfalkai@gmail.com>
2020-05-09 19:38:05 +0200

    post and post creator added to home, and wired all together

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, HostListener, } from '@angular/core';
+import { WLPost } from '../post/post.model';
+import { PostService } from '../post/post.service';
+
 
 
 @Component({
@@ -8,15 +11,55 @@ import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef } fro
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
-  @ViewChild('map', {static: true}) mapRef: ElementRef;
-  @ViewChild('search', {static: true}) searchRef: ElementRef;
-  map: google.maps.Map;
-  tmpMarker: google.maps.places.PlaceResult;
+  posts: WLPost[] =[];
+  likedPosts: Promise<string[]>;
+  lastPost: any = {
+    author: null,
+    comments: null,
+    date: null,
+    description: null,
+    followers: null,
+    id: null,
+    likes: null,
+    photoURLs: null,
+  };
+  loaded = false;
+  noMorePosts = false;
 
-
-  constructor() { }
-  ngOnInit(): void {
+  @HostListener('scroll', ['$event.target'])
+  onElementScroll(elem): void {
+    if ((elem.offsetHeight + elem.scrollTop*1.1) >=  elem.scrollHeight && !this.loaded && !this.noMorePosts) {
+      this.loaded = true;
+      this.loadNewsFeed();
+      setTimeout(() => this.loaded = false, 2000);
+    }
   }
+  constructor(private ps: PostService, private cd: ChangeDetectorRef) {
+  }
+  ngOnInit()  {
+    this.likedPosts = this.ps.getLikedPosts();
+    this.loadNewsFeed();
+  }
+
+  async likeEventReciever(event: {post: WLPost, isLike: boolean}) {
+    event.isLike ? this.ps.like(event.post) : this.ps.dislike(event.post);
+  }
+
+  loadNewsFeed() {
+    this.ps.loadNewsFeed(this.lastPost).then(docs => {
+      if (docs.length === 0 ){
+        this.noMorePosts = true;
+        this.cd.detectChanges();
+        return;
+      }
+      this.lastPost = docs[docs.length - 1];
+      docs.map(doc => this.posts.push(WLPost.deserialize(doc)));
+      this.cd.detectChanges();
+    });
+
+
+  }
+
 
 
 
