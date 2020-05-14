6b72b2a359def111d52ec6c7abd97deaef953518
Falkai Péter <formal.pfalkai@gmail.com>
2020-05-14 13:04:33 +0200

    image uploader logic and component

--- src/app/feature/image-uploader/image-uploader.module.ts
+++ src/app/feature/image-uploader/image-uploader.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageUploaderComponent } from './image-uploader.component';
 import { DropZoneDirective } from './drop-zone.directive';
+import { UploadTaskComponent } from './upload-task/upload-task.component';
+import { MaterialModule } from '@wl-shared/material.module';
 
 
 
 @NgModule({
-  declarations: [ImageUploaderComponent, DropZoneDirective],
+  declarations: [ImageUploaderComponent, DropZoneDirective, UploadTaskComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [
+    ImageUploaderComponent
   ]
 })
 export class ImageUploaderModule { }
